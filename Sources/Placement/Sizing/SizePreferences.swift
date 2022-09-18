c36437b41d164646dce7649fbc779340279590c7
Sam Pettersson <sam@sampettersson.com>
2022-09-18 11:18:15 +0200

    Remove some disused preferencekeys

--- Sources/Placement/Sizing/SizePreferences.swift
+++ Sources/Placement/Sizing/SizePreferences.swift
@@ -1,14 +1,6 @@
 import Foundation
 import SwiftUI
 
-struct SizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
-
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-        value = nextValue()
-    }
-}
-
 struct ChildrenIntrinsicSizesKey: PreferenceKey {
     static var defaultValue: [AnyHashable: CGSize] = [:]
 
@@ -17,14 +9,5 @@ struct ChildrenIntrinsicSizesKey: PreferenceKey {
         value = value.merging(nextValue, uniquingKeysWith: { _, rhs in
             rhs
         })
-    }
-}
-
-struct AccumulatedSizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
-
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-        let nextValue = nextValue()
-        value = CGSize(width: value.width + nextValue.width, height: value.height + nextValue.height)
     }
 }
