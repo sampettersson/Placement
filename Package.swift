1402d89f277fc73e03969c3e92b578e6d1a4bff3
Sam Pettersson <sam@sampettersson.com>
2022-09-22 13:12:28 +0200

    Exclude docc

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         .target(
             name: "Placement",
             dependencies: [
-            ]
+            ],
+            exclude: ["Placement.docc"]
         )
     ]
 )
