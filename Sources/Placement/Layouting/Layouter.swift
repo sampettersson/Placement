a97a7c1346372cfe1f9be426533c93f5e00d1cf1
Sam Pettersson <sam@sampettersson.com>
2022-10-04 09:00:15 +0200

    Rename function argument

--- Sources/Placement/Layouting/Layouter.swift
+++ Sources/Placement/Layouting/Layouter.swift
@@ -29,8 +29,8 @@ struct Layouter<Content: View, L: PlacementLayout>: View {
         }
         .environmentObject(coordinator)
         .environmentObject(coordinator.placementsCoordinator)
-        .transformPreference(PlacementIntrinsicSizesPreferenceKey.self) { anchors in
-            anchors = [:]
+        .transformPreference(PlacementIntrinsicSizesPreferenceKey.self) { intrinsicSizes in
+            intrinsicSizes = [:]
         }
     }
 }
